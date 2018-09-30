6bd7cf388f22318e1e629967b66429dcc566f671
Amit Kumar Tiwari <epsileer@gmail.com>
2018-09-30 22:08:09 +0530

    Add files via upload

--- Alibaba_cloud_server_code/codechef.sql
+++ Alibaba_cloud_server_code/codechef.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 30, 2018 at 07:50 PM
+-- Generation Time: Oct 01, 2018 at 12:35 AM
 -- Server version: 5.7.23-0ubuntu0.16.04.1
 -- PHP Version: 7.0.32-0ubuntu0.16.04.1
 
@@ -31,13 +31,6 @@ CREATE TABLE `Admin` (
   `accesstoken` varchar(100) NOT NULL,
   `refreshtoken` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `Admin`
---
-
-INSERT INTO `Admin` (`username`, `accesstoken`, `refreshtoken`) VALUES
-('admin', '536a0c45dc56329beef7e05c5de2c0b47e4424d1', '49c40b999375d5e8eec82abe9ea9a4b7d5126abd');
 
 -- --------------------------------------------------------
 
@@ -73,22 +66,6 @@ CREATE TABLE `event` (
   `status` int(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `event`
---
-
-INSERT INTO `event` (`username`, `eventcode`, `status`) VALUES
-('epilux', 'CCWC2018', NULL),
-('epilux', 'IEMCO6', NULL),
-('epsileer', 'MITC2018', NULL),
-('sahushivam', 'CCWC2018', NULL),
-('sahushivam', 'COOK98', NULL),
-('sahushivam', 'MITC2018', NULL),
-('subodhrai', 'LTIME64', NULL),
-('subodhrai', 'MITC2018', NULL),
-('subodhrai898', 'ELE32018', NULL),
-('subodhrai898', 'IEMCO6', NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -108,11 +85,10 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`username`, `name`, `device`, `email`) VALUES
 ('epilux', NULL, 'dhvuD7JEdh4:APA91bEjujPH5banxehQSD4cOZ0Tvl4Q77N6krXybwR_9nskEvcU29aFQNunucJoPeQcmLloJWfn61z5UjI4OxToiz4iMp1HhfYL3OpY017_OOopW-h5wWDIcdg6GFPfx_bS-rnN0gMN', NULL),
-('epsile', NULL, 'kdsjfakdsjfa', NULL),
-('epsileer', NULL, 'cO9GKxwmqiA:APA91bHHx785E5AJKYuOM8xb6qPhVrlln2fRZ7Uc6ckQoxGSSmmdzNg7fAyi2PlkTBxX_OjihdzpOeup7Bsm3RLCL7neaZyQuuliQbKe81xZE68WsZDX-LzAEF_vCbuCaYMfA4puuH-g', NULL),
+('epsileer', NULL, 'etb7-FnrHls:APA91bE1MmyneFyJl-CJU1PWzZ0PYhVZlfeu8WYgI7jREc_dBite4i_Opex7lEcCwn_1WBT5PVMdHmwvOIWnBuQW1lb3uMQn_HF-syeqjQJB8Jp7sE3lZ6dJQOzoAFTZPnR_-C77Wm58', NULL),
 ('sahushivam', NULL, 'dsrCzYd9IH0:APA91bGWo1abdYEIR3_JitRjK0O_B2PqdNFJG-DXL919A-HNira-KSfVE7zvswf5PWfoWZt90SMLckJ12jB77uP-TcNC1Isx01b5K8YOs1Pj9djuARzllPpH4_KGw5IncWdhD6FcAKDY', NULL),
 ('subodh898', NULL, 'fx419Q15qMM:APA91bHEo94l8DEFX7vVgS-mvFPRSsQgNVUw-q_FXRw9jz_SDxgxmPacQIVR5u6zKJKW1Ofj9Pd9ui-cSOjkIpKLcaG7AR4cYKjeffGIaB00t_-0AvortQBwVs8PkYTVfGhw4ri9XnRN', NULL),
-('subodhrai', NULL, 'f3h4HY5wUMU:APA91bFwfy9vBzc-EV8fhsYCbBB1cSNMeEkr6pVOP9fqnRh22ADkRCAiHixp8MSSyuzpsvMhbkRLowVOVS9AjYAoIJtJsjF96xC2Ml7DRp8_MY7_wHcqJSiQ4ZH-c3g1BWNnNXeaL3yn', NULL),
+('subodhrai', NULL, 'd-yGKIbJFZI:APA91bFH8ki7z3ZyiwWjmMAVhcnea-drjBgbn9ROY_hX1eUVShZ-7Toy1BYy-pkzGRV9lt6fMDwC-TwKzRFB2ebal_Si_EawkdY701xObs4OjenOIo4D7XfR-X_7wiJ5djEePhm38zxO', NULL),
 ('subodhrai898', NULL, 'ceDeYx7pa9w:APA91bH-nYk-4NICLgJAv3iEycoo4WERzVqUkfkkFN9OSgSHp2olliAnv_Z0_9edXX9gSi0WacYIMP2Yzy7bLsQWmJUBMm4_fzkk4wsCcIneC6j0l4t2nQDX1j20EZGVzweQb2qKuWAo', NULL);
 
 --
